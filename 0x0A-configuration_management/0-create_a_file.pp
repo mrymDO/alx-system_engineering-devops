db38a37e15e50aefb55cc1b06623ccc02484cae3
Meryem <dobli.meryeme@gmail.com>
2023-08-25 11:54:48 -0700

    task1

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Define a file resource with the specified properties
+file { '/tmp/school':
+  ensure  => present,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
