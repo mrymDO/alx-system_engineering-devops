0ac86acc789ddb29a3ee38389adeaa8e837fb427
Meryem <dobli.meryeme@gmail.com>
2023-09-25 11:56:06 -0700

    task1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Exports to CSV
+"""
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    employee_id = sys.argv[1]
+
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    todo_url = (
+            f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+            )
+
+    response = requests.get(user_url)
+    username = response.json().get('username')
+
+    response = requests.get(todo_url)
+    todo = response.json()
+    with open(f"{employee_id}.csv", "w") as csvfile:
+        for task in todo:
+            completed = task.get('completed')
+            title = task.get('title')
+            csvfile.write(f"{employee_id}, {username}, {completed}, {title}\n")
