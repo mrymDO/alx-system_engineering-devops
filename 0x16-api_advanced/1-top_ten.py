31f86a342afe2c1ea6a6317cdd8c32b6b4f4114b
Meryeme <dobli.meryeme@gmail.com>
2023-10-10 07:03:48 -0700

    fix6 task1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,7 +6,7 @@ import requests
 
 def top_ten(subreddit):
     """print titles of first 10 posts in a subreddit"""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {"User-Agent": "My-Agent"}
     allow_redirects = False
     response = requests.get(
@@ -15,7 +15,7 @@ def top_ten(subreddit):
     if response.status_code == 200:
         try:
             data = response.json()
-            posts = data.get("data").get("children")[:10]
+            posts = data.get("data").get("children")
 
             for post in posts:
                 title = post.get("data").get("title")
