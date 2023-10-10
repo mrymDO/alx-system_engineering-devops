a4a8fd8f1144459238159685c8543fa6690dc437
Meryeme <dobli.meryeme@gmail.com>
2023-10-10 04:50:24 -0700

    fix task0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,8 +11,12 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'MyRedditBot/1.0'}
     response = requests.get(url, headers=headers)
-    if response.status_code != 200:
+    if response.status_code == 200:
+        try:
+            data = response.json()
+            subscribers_count = data['data']['subscribers']
+            return subscribers_count
+        except KeyError:
+            return 0
+    else:
         return 0
-    data = response.json()
-    total_subscribers = data['data']['subscribers']
-    return total_subscribers
