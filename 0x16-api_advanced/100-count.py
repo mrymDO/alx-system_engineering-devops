d8fa018c61aae6f90203c1fc5aee148d515cdb56
Meryeme <dobli.meryeme@gmail.com>
2023-10-10 11:05:50 -0700

    fix 9  task 3

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -8,6 +8,9 @@ def count_words(subreddit, word_list, counts=None, after=None):
     if counts is None:
         counts = {}
 
+    if subreddit is None:
+        return
+
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {"User-Agent": "My-Agent"}
     params = {"after": after} if after else {}
@@ -19,21 +22,23 @@ def count_words(subreddit, word_list, counts=None, after=None):
         return
 
     data = response.json()
-    posts = data.get("data", {}).get("children", [])
+    children = data.get("data", {}).get("children", [])
 
-    for post in posts:
-        title = post.get("data", {}).get("title", "").lower()
+    for child in children:
+        title = child.get("data", {}).get("title", "").lower()
+
         for keyword in word_list:
             keyword = keyword.lower()
-            if title.count(keyword):
-                counts[keyword] = counts.get(keyword, 0) + title.count(keyword)
+            if keyword in title:
+                counts[keyword] = counts.get(keyword, 0) + 1
 
     new_after = data.get("data", {}).get("after")
+
     if new_after:
-        return count_words(subreddit, word_list, counts, after=new_after)
+        return count_words(subreddit, word_list, counts, new_after)
+    else:
+        sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
 
-    sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
-
-    for keyword, count in sorted_counts:
-        if count > 0:
+        for keyword, count in sorted_counts:
             print(f"{keyword}: {count}")
+        return
