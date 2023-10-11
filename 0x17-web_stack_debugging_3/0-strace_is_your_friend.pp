91799635294d520c3eae79b5bc6ea80a962e0257
mrym <dobli.meryeme@gmail.com>
2023-10-11 12:37:44 +0000

    task0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fix extension typo "phpp" to "php"
 
-exec{'fix-wordpress':
+exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin:/bin:/usr/bin'
+  path    => '/usr/local/bin/:/bin/'
 }
